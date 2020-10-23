f88ed2339563039e9c4d58e7a734579afdba8f1c
purvatechprescient <purva@techprescient.com>
2020-10-23 17:58:40 +0530

    First

--- Assignment1.js
+++ Assignment1.js
@@ -1,33 +1,37 @@
-function miss_element(arr1,arr2)
-{
-    var count=arr1.length;
-    for(var i=0;i<count;i++)
-    {
+function getElement(largeArray, smallArray) {
+    let flag = 0;
+    let iterate=[0,1,2,3,4]
+    for (i in iterate) {
 
-        if(arr2.indexOf(arr1[i])==-1)
-        {
-                return arr1[i];
-        
-
+        if (smallArray.indexOf(largeArray[i]) == -1) {
+            console.log(largeArray[i]);
+            flag = 1;
+            break;
         }
+
+    }
+    if (flag === 0) {
+        console.log("No missing element found");
+    }
+
+
+}
+
+function main() {
+    let arr1 = [1, 4, 2, 5, 3];
+    let arr2 = [5, 4, 1, 2];
+
+    let count1 = arr1.length;
+    let count2 = arr2.length;
+
+    if (count1 >= count2) {
+        getElement(arr1, arr2);
+
+    }
+    else {
+        getElement(arr2, arr1);
     }
 
 }
 
-var arr1=[1,4,2,5,3];
-var arr2=[5,4,1,2];
-
-var count1=arr1.length;
-var count2=arr2.length;
-
-if(count1>count2)
-{
-    var element=miss_element(arr1,arr2);
-
-}
-else
-{
-    var element=miss_element(arr2,arr1);
-}
-console.log(element);
-
+main();
